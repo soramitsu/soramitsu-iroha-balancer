e54409f9468153237ecd8001f2a63e1ead20b788
kgusman <kamill.gusmanov@gmail.com>
2020-08-27 14:16:57 +0300

    Small fix on container test

--- iroha-balancer-core/src/test/java/soramitsu/irohautils/balancer/IrohaBalancerCoreContainerTest.kt
+++ iroha-balancer-core/src/test/java/soramitsu/irohautils/balancer/IrohaBalancerCoreContainerTest.kt
@@ -1,21 +1,17 @@
 package soramitsu.irohautils.balancer
 
-import iroha.protocol.TransactionOuterClass
-import jp.co.soramitsu.iroha.java.Transaction
-import jp.co.soramitsu.iroha.java.Utils
-import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
 import org.junit.Assert
-import org.junit.ClassRule
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.containers.Network
+import soramitsu.irohautils.balancer.TestContainersMock.rmqConfig
 import soramitsu.irohautils.balancer.helper.ContainerHelper
-import java.util.*
-import javax.xml.bind.DatatypeConverter
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ContextConfiguration(initializers = [TestContainersMock.Initializer::class])
 class IrohaBalancerCoreContainerTest {
 
     private val containerHelper = ContainerHelper()
@@ -26,18 +22,12 @@ class IrohaBalancerCoreContainerTest {
     private val irohaBalancerCoreContainer =
             containerHelper.createIrohaBalancerCoreContainer(irohaBalancerCoreContextFolder, irohaBalancerCoreDockerfile)
                     .withNetwork(Network.SHARED)
-
-    @ClassRule
-    val rabbitMq: RabbitMqContainer = RabbitMqContainer("rabbitmq:management")
-            .withExposedPorts(5672)
-
     @BeforeAll
     fun init() {
-        rabbitMq.start()
 
         irohaBalancerCoreContainer
-                .withEnv("CAMEL_COMPONENT_RABBITMQ_HOSTNAME", rabbitMq.host)
-                .withEnv("CAMEL_COMPONENT_RABBITMQ_PORT_NUMBER", rabbitMq.getMappedPort(5672).toString())
+                .withEnv("CAMEL_COMPONENT_RABBITMQ_HOSTNAME", rmqConfig.host)
+                .withEnv("CAMEL_COMPONENT_RABBITMQ_PORT_NUMBER", rmqConfig.port.toString())
                 .withEnv("CAMEL_COMPONENT_RABBITMQ_USERNAME", "guest")
                 .withEnv("CAMEL_COMPONENT_RABBITMQ_PASSWORD", "guest")
                 .start()
@@ -52,5 +42,4 @@ class IrohaBalancerCoreContainerTest {
     fun testContainerRunning() {
         Assert.assertTrue(irohaBalancerCoreContainer.isRunning())
     }
-
 }
