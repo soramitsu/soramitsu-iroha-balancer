5f57bc2089e0bb3c459329c67af3ee10e88ef94b
kgusman <kamill.gusmanov@gmail.com>
2020-09-09 01:13:39 +0300

    Add basic test

--- iroha-balancer-integration-test/src/test/kotlin/soramitsu/iroha/balancer/integration_test/IntegrationTests.kt
+++ iroha-balancer-integration-test/src/test/kotlin/soramitsu/iroha/balancer/integration_test/IntegrationTests.kt
@@ -1,5 +1,13 @@
 package soramitsu.iroha.balancer.integration_test
 
+import iroha.protocol.Endpoint.TxStatus
+import iroha.protocol.TransactionOuterClass
+import jp.co.soramitsu.crypto.ed25519.Ed25519Sha3
+import jp.co.soramitsu.crypto.ed25519.Utils
+import jp.co.soramitsu.iroha.java.IrohaAPI
+import jp.co.soramitsu.iroha.java.Transaction
+import jp.co.soramitsu.iroha.java.Utils.toHexHash
+import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -7,7 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.containers.Network
 import org.testcontainers.junit.jupiter.Testcontainers
+import soramitsu.iroha.balancer.integration_test.TestContainersMock.network
+import soramitsu.irohautils.balancer.client.config.RMQConfig
+import soramitsu.irohautils.balancer.client.service.IrohaBalancerClientService
 import soramitsu.irohautils.balancer.helper.ContainerHelper
+import javax.xml.bind.DatatypeConverter
 
 @Testcontainers
 @SpringBootTest
@@ -24,6 +36,9 @@ class IntegrationTests {
             containerHelper.createIrohaBalancerCoreContainer(irohaBalancerCoreContextFolder, irohaBalancerCoreDockerfile)
                     .withNetwork(Network.SHARED)
 
+    val crypto = Ed25519Sha3()
+    private lateinit var client: IrohaBalancerClientService
+
     @BeforeAll
     fun init() {
         irohaBalancerCoreContainer
@@ -32,13 +47,37 @@ class IntegrationTests {
                 .withEnv("CAMEL_COMPONENT_RABBITMQ_USERNAME", "guest")
                 .withEnv("CAMEL_COMPONENT_RABBITMQ_PASSWORD", "guest")
                 .start()
+
+        val rmqConfig = RMQConfig(
+                TestContainersMock.rabbitmq.host,
+                TestContainersMock.rabbitmq.getMappedPort(5672),
+                "guest",
+                "guest"
+        )
+        client = IrohaBalancerClientService(rmqConfig)
     }
 
     @Test
     fun dummy() {
-        println(TestContainersMock.rabbitmq.getMappedPort(5672))
-        println(TestContainersMock.network.peers)
-        println(irohaBalancerCoreContainer.getContainerInfo())
-    }
+        val transaction: TransactionOuterClass.Transaction = Transaction.builder(defaultAccountId)
+                .createAccount("account_", defaultDomainName, crypto.generateKeypair().getPublic())
+                .build()
+                .sign(defaultKeyPair)
+                .build()
+        client.balanceToTorii(transaction)
 
+        val iroha = IrohaAPI(network.toriiAddresses[0])
+        checkCommitted(toHexHash(transaction), iroha)
+    }
+    private fun checkCommitted(trxHash1: String, iroha: IrohaAPI) {
+        var txStatus = iroha.txStatusSync(trxHash1.toByteArray()).txStatus
+        while (TxStatus.COMMITTED != txStatus) {
+            try {
+                Thread.sleep(500)
+            } catch (e: InterruptedException) {
+            }
+            txStatus = iroha.txStatusSync(trxHash1.toByteArray()).txStatus
+            println("Status for $trxHash1: $txStatus")
+        }
+    }
 }
